847ce676ee3bc0c9bf3658e682927fe093f6f27c
Rory Gilmore <naima.adanahmed@gmail.com>
2020-02-11 12:53:37 +0100

    added checks in the users.js file

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 
 connectDB();
 
+app.use(express.json({extended:false}))
+
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
