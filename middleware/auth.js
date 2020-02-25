014270d863e4e270edc11b3227e7ea9c229a35ca
Rory Gilmore <naima.adanahmed@gmail.com>
2020-02-25 14:45:39 +0100

    created post contacts route

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ module.exports = function (req, res, next){
   const token= req.header('x-auth-token');
   //check if not token
   if(!token){
-    return res.status(401).json({msg: 'No token, authoriztion denied'})
+    return res.status(401).json({msg: 'No token, authorization denied'})
   }
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'))
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const Contact = require('../models/Contact');
@@ -11,21 +11,53 @@ const Contact = require('../models/Contact');
 
 router.get('/', auth, async (req, res) => {
   try {
-    const contacts= await Contact.find({user: req.user.id}).sort({date: -1})
-    res.json(contacts)
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    res.json(contacts);
   } catch (err) {
-    console.error(err.message)
-    res.status(500).send('Server error')
-    
+    console.error(err.message);
+    res.status(500).send('Server error');
   }
 });
 
 //@route api/contacts
 //@desc add new contacts
 //@access private
-router.post('/', (req, res) => {
-  res.send('add contact');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+      return res.status(400).json({errors: errors.array()})
+    }
+    const {name, email, phone, type } = req.body;
+    try {
+      const newContact= new Contact({
+        name, 
+        email,
+        phone,
+        type,
+        user: req.user.id
+    }) 
+    
+    const contact = await newContact.save();
+    res.json(contact)
+  }catch (err) {
+      console.error(err.message);
+      res.status(500).send('server error')
+    }
+   
+  }
+);
 
 //@route api/contacts/:id
 //@desc update new contact
