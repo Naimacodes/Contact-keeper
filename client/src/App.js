7d1bc118142e54aaeb7a38bb45a72c60f3eb75ec
Rory Gilmore <naima.adanahmed@gmail.com>
2020-03-17 08:26:14 +0100

    added AuthState to App.js and set up initial State in AuthState

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
-import Home from './components/pages/Home'
-import About from './components/pages/About'
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import ContactState from './context/contact/ContactState'
+import ContactState from './context/contact/ContactState';
+import AuthState from './context/Auth/AuthState';
 import './App.css';
 
 function App() {
   return (
-    <ContactState>
-    <Router>
-      <Fragment>
-        <Navbar />
-      </Fragment>
-      <div className="container">
-        <Switch>
-          <Route exact path = '/' component={Home}></Route>
-          <Route exact path = '/about' component={About}></Route>
-        </Switch>
-      </div>
-    </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+          </Fragment>
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Home}></Route>
+              <Route exact path='/about' component={About}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -3,19 +3,73 @@ import React, { useReducer } from 'react';
 import AuthReducer from './authReducer';
 import AuthContext from './authContext';
 import axios from 'axios';
-import {} from '../types';
+import {REGISTER_FAIL,
+ USER_LOADED,
+ AUTH_ERROR,
+ LOGIN_SUCCESS,
+LOGIN_FAIL,
+ LOGOUT,
+ CLEAR_ERRORS,
+ REMOVE_ALERT
+} from '../types';
 
-const Authstate = props => {
+const AuthState = props => {
 const initialState = {
+  token : localStorage.getItem('token'),
+  isAuthenticated : null,
+  loading: true,
+  user: null,
+  error: null
+
 
 };
 
 const [state, dispatch] = useReducer(AuthReducer, initialState);
 
+// Load User
+//Checks which user logs in , will hit the auth endpoint and get the data
+
+
+
+// register User
+//sign the user up, get the token back
+
+
+
+//Login User
+//Log the user in get the token
+
+
+
+// Logout
+// Destroys the token logs the user out
+
+
+
+
+// Clear Errors
+//clear any errors in the state
+
+
+
+
+
+
+
 
 return (
 <AuthContext.Provider
-value={{}}
+value={{
+
+  token: state.token,
+  isAuthenticated: state.isAuthenticated,
+  login: state.login,
+  user: state.user,
+  error: state.error,
+
+
+
+}}
 
 >
 {props.children}
@@ -24,4 +78,4 @@ value={{}}
 }
 
 
-export default Authstate
+export default AuthState
