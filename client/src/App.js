b1851c30fb2d69925ad3b30a1d162b51ec58c94a
Rory Gilmore <naima.adanahmed@gmail.com>
2020-03-17 08:57:51 +0100

    added register route in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -19,6 +20,8 @@ function App() {
             <Switch>
               <Route exact path='/' component={Home}></Route>
               <Route exact path='/about' component={About}></Route>
+              <Route exact path='/register' component={Register}></Route>
+
             </Switch>
           </div>
         </Router>
