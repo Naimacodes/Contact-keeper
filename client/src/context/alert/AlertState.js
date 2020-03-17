918eff1908b73fcc5b745d1b634305110f7acee8
Rory Gilmore <naima.adanahmed@gmail.com>
2020-03-17 11:30:47 +0100

    fixed error uuid in AlertState and no-unused-expression error  on reducer

--- client/src/context/alert/AlertState.js
+++ client/src/context/alert/AlertState.js
@@ -1,49 +1,41 @@
-import React from 'react';
 import React, { useReducer } from 'react';
 import AlertReducer from './alertReducer';
 import AlertContext from './alertContext';
 import { v4 as uuidv4 } from 'uuid';
-import {SET_ALERT, REMOVE_ALERT} from '../types';
+import { SET_ALERT, REMOVE_ALERT } from '../types';
 
 const AlertState = props => {
-const initialState = [
+  const initialState = [];
 
-];
+  const [state, dispatch] = useReducer(AlertReducer, initialState);
 
-const [state, dispatch] = useReducer(AlertReducer, initialState);
+  //set Alert
+  const setAlert = (msg, type, timeout = 5000) => {
+    const id = uuidv4();
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, type, id }
+    });
 
-//set Alert
-const setAlert = (msg, type, timeout = 5000) => {
-const id = uuid.v4()
-dispatch({
-  type: SET_ALERT,
-  payload: {msg, type, id}
-})
+    setTimeout(
+      () => dispatch({ type: REMOVE_ALERT, payload: id }),
 
-setTimeout(() => dispatch({type: REMOVE_ALERT, payload: id})
-  
-, timeout);
-}
+      timeout
+    );
+  };
 
+  //remove alert
 
-//remove alert 
+  return (
+    <AlertContext.Provider
+      value={{
+        alerts: state,
+        setAlert
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
+  );
+};
 
-
-
-
-
-return (
-<AlertContext.Provider
-value={{
-  alerts: state,
-  setAlert
-}}
-
->
-{props.children}
-</AlertContext.Provider>
-)
-}
-
-
-export default AlertState
+export default AlertState;
