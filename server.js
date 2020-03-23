aac75894c75357a31e187f1ba8bb140dd76c649d
Rory Gilmore <naima.adanahmed@gmail.com>
2020-03-23 09:58:17 +0100

    preparing for deployment

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 connectDB();
 
-app.use(express.json({extended:false}))
+app.use(express.json({ extended: false }));
 
+//Defines Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'welcome to the contact keeper api' })
-);
+// Serves static assets (react) in production
 
-const PORT = process.env.PORT|| 5000;
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('server listening on port 5000'));
